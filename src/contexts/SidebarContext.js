1a60eb923bd63ba5509b77dc20b836102bb3bfa3
antonioalexandre1984 <antonioalexandre1984@gmail.com>
2022-12-13 15:23:20 -0300

    App Finished

---
+++ src/contexts/SidebarContext.js
@@ -0,0 +1,24 @@
+import React,{ createContext,useState } from 'react';
+
+export const SidebarContext = createContext();
+
+const SidebarProvider = ({ children }) => {
+    // sidebar state
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const handleClose = () => {
+        setIsOpen(false);
+    }
+
+   /*  const handleOpen = () => {
+        setIsOpen(true);
+    } */
+
+    return (
+        <SidebarContext.Provider value={{isOpen,setIsOpen,handleClose}}>
+            {children}
+        </SidebarContext.Provider>
+    )
+}
+ 
+export default SidebarProvider;
